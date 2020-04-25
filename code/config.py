8ea6a39f443a886b31875fb5ced28994e06bc04d
osc <gpetry37@gmail.com>
2020-04-25 14:41:58 -0400

    Updated config file heading

--- code/config.py
+++ code/config.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 
 """
-CSC 315
-Spring 2020
-Group 4 - Gordon Petry, Jason Kantner, Michael Williams, Casey Lishko
+CSC 315 Project
+Group 4 - CivicStats
+Gordon Petry, Jason Kantner, Michael Williams, Casey Lishko
 
 This code is from:
 https://www.postgresqltutorial.com/postgresql-python/
 """
 
 from ConfigParser import ConfigParser
- 
- 
+
+
 def config(filename='database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
     parser.read(filename)
- 
+
     # get section, default to postgresql
     db = {}
     if parser.has_section(section):
@@ -26,5 +26,5 @@ def config(filename='database.ini', section='postgresql'):
             db[param[0]] = param[1]
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
- 
+
     return db
